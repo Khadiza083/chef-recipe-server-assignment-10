70371c11c1aaad8706284a77a1c9e8e21e8aa01d
Khadiza083 <khadizakhatun4083@gmail.com>
2023-05-05 23:02:36 +0600

    server site running

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const chefInfo = require('./data/data.json')
 
 app.get('/', (req, res) =>{
     res.send('site is running')
+})
+
+app.get('/chefInfo', (req, res) =>{
+    res.send(chefInfo)
 })
 
 app.listen(port, ()=>{
